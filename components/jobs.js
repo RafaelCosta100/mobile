5e4113f9e0bf29d9be7b6dadb6ec0c6604712d5d
RafaelCosta100 <rafa_sc@hotmail.com>
2026-04-08 20:24:13 -0300

    exercicios 9 e 10

---
+++ components/jobs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, Image} from 'react-native';
+export default function Jobs(props){
+    return(
+        <view>
+            <Image
+            source={{ uri: props.img}}
+            style={{width: props.largura, height: props.altura}}
+            />
+            <Text>{props.fulano}</Text>
+        </view>
+    );
+};
